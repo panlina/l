0dd649d71d0ff15d6919ba77d402809a9fd9fdfb
Jack Lu <jacklu@jacklu.me>
2020-08-15 23:44:31 +0800

    export Statement

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.parse = require('./parse');
 exports.compile = require('./compile');
 exports.generate = require('./generate');
 exports.Expression = require('./Expression');
+exports.Statement = require('./Statement');
