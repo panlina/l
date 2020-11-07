5c971898b32e9bb13b5911c5a17a2fbc1af2a611
Jack Lu <jacklu@jacklu.me>
2020-11-07 23:56:58 +0800

    +string

--- semantics.js
+++ semantics.js
@@ -3,6 +3,9 @@ var Expression = require('./Expression');
 var Statement = require('./Statement');
 var semantics = grammar.createSemantics().addOperation('parse', {
 	number: x => new Expression.Literal(+x.sourceString),
+	string: (open, x, close) => new Expression.Literal(x.children.map(char => char.parse()).join('')),
+	char_literal: x => x.sourceString,
+	char_escaped: (backslash, x) => escape[x.sourceString],
 	identifier: (_, x) => x.sourceString,
 	ExpressionName: identifier => new Expression.Name(identifier.parse()),
 	ExpressionAtom_parentheses: (open, expression, close) => expression.parse(),
@@ -13,4 +16,14 @@ var semantics = grammar.createSemantics().addOperation('parse', {
 	Program_expression: (expression, end) => expression.parse(),
 	Program_statement: (statement, end) => statement.children.map(s => s.parse())
 });
+var escape = {
+	'"': '"',
+	'\\': '\\',
+	b: '\b',
+	f: '\f',
+	n: '\n',
+	r: '\r',
+	t: '\t',
+	v: '\v'
+};
 module.exports = semantics;
