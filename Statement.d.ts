26de1c1b0b3cba40ec587f17a4a9fc2c4d96ebd5
Jack Lu <jacklu@jacklu.me>
2020-11-12 00:01:59 +0800

    Statement.d.ts: +statement subtypes

--- Statement.d.ts
+++ Statement.d.ts
@@ -1,6 +1,19 @@
+import * as Expression from './Expression';
+
 class Statement {
 	constructor(type: string);
 	type: string;
 }
 
 export = Statement;
+
+export class Assign extends Statement {
+	constructor(left: string, right: Expression);
+	left: string;
+	right: Expression;
+}
+
+export class Placeholder extends Expression {
+	constructor(name: string);
+	name: string;
+}
