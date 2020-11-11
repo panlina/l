a00f7ad78e023d7f25c09e4f6b0222771814fde8
Jack Lu <jacklu@jacklu.me>
2020-11-11 23:57:55 +0800

    Expression.d.ts: +expression subtypes

--- Expression.d.ts
+++ Expression.d.ts
@@ -4,3 +4,39 @@ class Expression {
 }
 
 export = Expression;
+
+export class Literal extends Expression {
+	constructor(value);
+	value;
+}
+
+export class Name extends Expression {
+	constructor(identifier: string);
+	identifier: string;
+}
+
+export class Object extends Expression {
+	constructor(property: { name: string, value: Expression }[]);
+	property: { name: string, value: Expression }[];
+}
+
+export class Array extends Expression {
+	constructor(element: Expression[]);
+	element: Expression[];
+}
+
+export class Tuple extends Expression {
+	constructor(element: Expression[]);
+	element: Expression[];
+}
+
+export class Call extends Expression {
+	constructor(expression: Expression, argument: Expression);
+	expression: Expression;
+	argument: Expression;
+}
+
+export class Placeholder extends Expression {
+	constructor(name: string);
+	name: string;
+}
