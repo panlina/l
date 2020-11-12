26fed8d70c9ef6db6b23d8ab56e613ef11a76e03
Jack Lu <jacklu@jacklu.me>
2020-11-12 22:56:23 +0800

    +Interpretation.d.ts

---
+++ Interpretation.d.ts
@@ -0,0 +1,16 @@
+import Expression from './Expression';
+interface Interpretation<T> {
+	expression: {
+		literal: (expression: Expression.Literal) => T;
+		name: (expression: Expression.Name) => T;
+		object: ($property: { name: string, value: T }[]) => T;
+		array: ($element: T[]) => T;
+		tuple: ($element: T[]) => T;
+		call: ($expression: T, $argument: T) => T;
+	};
+	statement: {
+		'[]': ($statement: T[]) => T;
+		assign: ($left: string, $right: T) => T;
+	};
+};
+export = Interpretation;
--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,6 @@ import * as l from './l';
 export = l;
 export * as Expression from './Expression';
 export * as Statement from './Statement';
+export * as Interpretation from './Interpretation';
 export * as parse from './parse';
 export * as generate from './generate';
