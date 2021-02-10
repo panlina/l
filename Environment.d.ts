df347836ca268a170899b77682ef1970da523314
Jack Lu <jacklu@jacklu.me>
2021-02-10 22:25:17 +0800

    Environment->Environment<T>

--- Environment.d.ts
+++ Environment.d.ts
@@ -1,10 +1,10 @@
 import * as Scope from './Scope';
 
-export = class Environment {
+export = class Environment<T> {
 	constructor(scope: Environment['scope'], parent: Environment['parent']);
-	scope: Scope;
-	parent: Environment | undefined;
+	scope: Scope<T>;
+	parent: Environment<T> | undefined;
 	resolve(name: string): [T, number] | undefined;
-	ancestor(depth: number): Environment;
-	push(scope: Scope): Environment;
+	ancestor(depth: number): Environment<T>;
+	push(scope: Scope<T>): Environment<T>;
 }
