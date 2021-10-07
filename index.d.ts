ef5c81d7e89ff88b4cd2f83a502d437610879ff2
Jack Lu <jacklu@jacklu.me>
2021-10-07 23:31:21 +0800

    index.d.ts: reorder exports

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 import * as l from './l';
 export = l;
+export * as parse from './parse';
+export * as compile from './compile';
+export * as generate from './generate';
 export * as Expression from './Expression';
 export * as Statement from './Statement';
 export * as Scope from './Scope';
 export * as Environment from './Environment';
 export * as Interpretation from './Interpretation';
-export * as parse from './parse';
-export * as compile from './compile';
-export * as generate from './generate';
 export * as global from './global';
