c44cc2812e2f5c0447a4d343c27f3509cf35b02e
Jack Lu <jacklu@jacklu.me>
2021-10-16 15:58:01 +0800

    +test/f.d.ts

---
+++ test/f.d.ts
@@ -0,0 +1,4 @@
+import * as Environment from '../Environment';
+import * as Interpretation from '../Interpretation';
+declare var i: Interpretation<(environment: Environment<any>) => any>;
+export = i;
