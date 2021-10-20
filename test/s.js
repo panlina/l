5973d9fb17c5920b7bb5cd8aaf1051fea6de0154
Jack Lu <jacklu@jacklu.me>
2021-10-20 17:55:25 +0800

    s: +null, boolean, number, string

--- test/s.js
+++ test/s.js
@@ -1,7 +1,11 @@
 var t = require("@babel/types");
 var i = {
 	expression: {
-		undefined: expression => t.unaryExpression("void", t.numericLiteral(0))
+		undefined: expression => t.unaryExpression("void", t.numericLiteral(0)),
+		null: expression => t.nullLiteral(),
+		boolean: expression => t.booleanLiteral(expression.value),
+		number: expression => t.numericLiteral(expression.value),
+		string: expression => t.stringLiteral(expression.value)
 	}
 };
 module.exports = i;
