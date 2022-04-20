085dd4ec87ca474007a1781c6b7e5e18891cb8bc
Jack Lu <jacklu@jacklu.me>
2022-04-20 23:18:18 +0800

    format test/case/goto.js, program.js

--- test/case/goto.js
+++ test/case/goto.js
@@ -1,5 +1,5 @@
 module.exports = {
-	environment: {a:0},
+	environment: { a: 0 },
 	program: "goto L; let a = a + 1; L: let a = a + 2;",
 	effect: { a: 2 }
 };
--- test/case/program.js
+++ test/case/program.js
@@ -1,5 +1,5 @@
 module.exports = {
-	environment: {a:0},
+	environment: { a: 0 },
 	program: "let a = 1; var b; let b = a + 1; let a = b;",
 	effect: { a: 2 }
 };
