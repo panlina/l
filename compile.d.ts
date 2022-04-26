97f794df4cd65a8586ae043bf92df93a4970a105
Jack Lu <jacklu@jacklu.me>
2022-04-26 23:58:07 +0800

    compile.d.ts: fix program parameter

--- compile.d.ts
+++ compile.d.ts
@@ -3,7 +3,7 @@ import * as Statement from './Statement';
 import * as Environment from './Environment';
 import * as Interpretation from './Interpretation';
 declare function compile<T>(
-	expression: Expression | Statement,
+	program: Expression | Statement[],
 	environment: Environment<'variable' | 'label'>,
 	interpretation: Interpretation<T>
 ): T;
