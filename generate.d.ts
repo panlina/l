9fb41dc75579a1f73cada067814b0938847f1964
Jack Lu <jacklu@jacklu.me>
2022-07-27 22:32:08 +0800

    generate.d.ts: Statement[]->(Statement | Statement.Label)[]

--- generate.d.ts
+++ generate.d.ts
@@ -1,4 +1,4 @@
 import Expression = require('./Expression');
 import Statement = require('./Statement');
-declare function generate(program: Expression | Statement | Statement[]): string;
+declare function generate(program: Expression | Statement | (Statement | Statement.Label)[]): string;
 export = generate;
