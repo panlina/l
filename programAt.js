f9777af00be87817fd59db6f64c53f1ec12a09c5
Jack Lu <jacklu@jacklu.me>
2022-08-12 23:57:42 +0800

    programAt: use children

--- programAt.js
+++ programAt.js
@@ -1,18 +1,16 @@
+var children = require('./children');
 function programAt(program, position) {
-	switch (typeof program) {
-		case 'object':
-			if ('node' in program)
-				if (
-					program.node.source.startIdx <= position
-					&&
-					program.node.source.endIdx >= position
-				)
-					var p = program;
-			for (var key in program) {
-				var q = programAt(program[key], position);
-				if (q) p = q;
-			}
-			return p;
+	if ('node' in program)
+		if (
+			program.node.source.startIdx <= position
+			&&
+			program.node.source.endIdx >= position
+		)
+			var p = program;
+	for (var child of children(program)) {
+		var q = programAt(child, position);
+		if (q) p = q;
 	}
+	return p;
 }
 module.exports = programAt;
