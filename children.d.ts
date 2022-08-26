95835f9a171f1bb568ae9a922e88354233f24c23
Jack Lu <jacklu@jacklu.me>
2022-08-26 16:03:56 +0800

    children.d.ts, traverse.d.ts: Statement[]->(Statement | Label)[]

--- children.d.ts
+++ children.d.ts
@@ -1,5 +1,5 @@
 import Expression = require('./Expression');
 import Statement = require('./Statement');
 import Label = require('./Label');
-declare function children(program: Expression | Statement[] | Statement | Label): (Expression | Statement[] | Statement | Label)[];
+declare function children(program: Expression | (Statement | Label)[] | Statement | Label): (Expression | (Statement | Label)[] | Statement | Label)[];
 export = children;
--- traverse.d.ts
+++ traverse.d.ts
@@ -1,5 +1,5 @@
 import Expression = require('./Expression');
 import Statement = require('./Statement');
 import Label = require('./Label');
-declare function traverse(program: Expression | Statement[] | Statement | Label): Generator<Expression | Statement[] | Statement | Label>;
+declare function traverse(program: Expression | (Statement | Label)[] | Statement | Label): Generator<Expression | (Statement | Label)[] | Statement | Label>;
 export = traverse;
