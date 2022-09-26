d1a3b2eefde019c0cc216cc950ac910c0e9d485d
Jack Lu <jacklu@jacklu.me>
2022-09-26 22:08:28 +0800

    test.machine.js: reorder tests

--- test/test.machine.js
+++ test/test.machine.js
@@ -16,76 +16,6 @@ describe('machine', function () {
 		machine.step();
 		assert(Value.equals(machine.evaluate(l`a`), new Value.Number(14)));
 	});
-	describe('assign', function () {
-		it('name', function () {
-			var machine = new Machine(
-				new Environment(new Scope({ a: new Value.Undefined() })),
-				l``
-			);
-			machine.execute(l`let a=12;`[0]);
-			assert(Value.equals(machine.evaluate(l`a`), new Value.Number(12)));
-		});
-		it('element', function () {
-			var machine = new Machine(
-				new Environment(new Scope({ array: new Value.Array([new Value.Number(12), new Value.Number(26)]) })),
-				l``
-			);
-			machine.execute(l`let array@1=28;`[0]);
-			assert(Value.equals(machine.evaluate(l`array`).element[1], new Value.Number(28)));
-		});
-		it('property', function () {
-			var machine = new Machine(
-				new Environment(new Scope({ object: new Value.Object({ a: new Value.Number(12), b: new Value.Number(26) }) })),
-				l``
-			);
-			machine.execute(l`let object.a=14;`[0]);
-			assert(Value.equals(machine.evaluate(l`object`).property['a'], new Value.Number(14)));
-		});
-		it('array', function () {
-			var machine = new Machine(
-				new Environment(new Scope({ a: new Value.Undefined(), b: new Value.Undefined() })),
-				l``
-			);
-			machine.execute(l`let [a,b]=[12,26];`[0]);
-			assert(Value.equals(machine.evaluate(l`a`), new Value.Number(12)));
-			assert(Value.equals(machine.evaluate(l`b`), new Value.Number(26)));
-		});
-		it('tuple', function () {
-			var machine = new Machine(
-				new Environment(new Scope({ a: new Value.Undefined(), b: new Value.Undefined() })),
-				l``
-			);
-			machine.execute(l`let {a,b}={12,26};`[0]);
-			assert(Value.equals(machine.evaluate(l`a`), new Value.Number(12)));
-			assert(Value.equals(machine.evaluate(l`b`), new Value.Number(26)));
-		});
-		it('object', function () {
-			var machine = new Machine(
-				new Environment(new Scope({ a: new Value.Undefined(), b: new Value.Undefined() })),
-				l``
-			);
-			machine.execute(l`let {a:a,b:b}={a:12,b:26};`[0]);
-			assert(Value.equals(machine.evaluate(l`a`), new Value.Number(12)));
-			assert(Value.equals(machine.evaluate(l`b`), new Value.Number(26)));
-		});
-	});
-	it('block', function () {
-		var machine = new Machine(
-			new Environment(new Scope({ a: new Value.Number(0), b: new Value.Number(1), c: new Value.Number(1) })),
-			l``
-		);
-		machine.execute(l`{var a;let a=1;let c=a+b;}`[0]);
-		assert(Value.equals(machine.evaluate(l`c`), new Value.Number(2)));
-	});
-	it('expression statement', function () {
-		var machine = new Machine(
-			new Environment(new Scope({ a: new Value.Number(1), reset: new Value.Undefined() })),
-			l``
-		);
-		machine.execute(l`let reset = (_ => (let a = 0;));`[0]);
-		machine.execute(l`reset 0;`[0]);
-		assert(Value.equals(machine.evaluate(l`a`), new Value.Number(0)));
-	});
 	it('array', function () {
 		var machine = new Machine(
 			new Environment(new Scope({})),
@@ -172,6 +102,76 @@ describe('machine', function () {
 		);
 		var a = machine.evaluate(l`({x,y}=>x+y){1,2}`);
 		assert(Value.equals(a, new Value.Number(3)));
+	});
+	describe('assign', function () {
+		it('name', function () {
+			var machine = new Machine(
+				new Environment(new Scope({ a: new Value.Undefined() })),
+				l``
+			);
+			machine.execute(l`let a=12;`[0]);
+			assert(Value.equals(machine.evaluate(l`a`), new Value.Number(12)));
+		});
+		it('element', function () {
+			var machine = new Machine(
+				new Environment(new Scope({ array: new Value.Array([new Value.Number(12), new Value.Number(26)]) })),
+				l``
+			);
+			machine.execute(l`let array@1=28;`[0]);
+			assert(Value.equals(machine.evaluate(l`array`).element[1], new Value.Number(28)));
+		});
+		it('property', function () {
+			var machine = new Machine(
+				new Environment(new Scope({ object: new Value.Object({ a: new Value.Number(12), b: new Value.Number(26) }) })),
+				l``
+			);
+			machine.execute(l`let object.a=14;`[0]);
+			assert(Value.equals(machine.evaluate(l`object`).property['a'], new Value.Number(14)));
+		});
+		it('array', function () {
+			var machine = new Machine(
+				new Environment(new Scope({ a: new Value.Undefined(), b: new Value.Undefined() })),
+				l``
+			);
+			machine.execute(l`let [a,b]=[12,26];`[0]);
+			assert(Value.equals(machine.evaluate(l`a`), new Value.Number(12)));
+			assert(Value.equals(machine.evaluate(l`b`), new Value.Number(26)));
+		});
+		it('tuple', function () {
+			var machine = new Machine(
+				new Environment(new Scope({ a: new Value.Undefined(), b: new Value.Undefined() })),
+				l``
+			);
+			machine.execute(l`let {a,b}={12,26};`[0]);
+			assert(Value.equals(machine.evaluate(l`a`), new Value.Number(12)));
+			assert(Value.equals(machine.evaluate(l`b`), new Value.Number(26)));
+		});
+		it('object', function () {
+			var machine = new Machine(
+				new Environment(new Scope({ a: new Value.Undefined(), b: new Value.Undefined() })),
+				l``
+			);
+			machine.execute(l`let {a:a,b:b}={a:12,b:26};`[0]);
+			assert(Value.equals(machine.evaluate(l`a`), new Value.Number(12)));
+			assert(Value.equals(machine.evaluate(l`b`), new Value.Number(26)));
+		});
+	});
+	it('block', function () {
+		var machine = new Machine(
+			new Environment(new Scope({ a: new Value.Number(0), b: new Value.Number(1), c: new Value.Number(1) })),
+			l``
+		);
+		machine.execute(l`{var a;let a=1;let c=a+b;}`[0]);
+		assert(Value.equals(machine.evaluate(l`c`), new Value.Number(2)));
+	});
+	it('expression statement', function () {
+		var machine = new Machine(
+			new Environment(new Scope({ a: new Value.Number(1), reset: new Value.Undefined() })),
+			l``
+		);
+		machine.execute(l`let reset = (_ => (let a = 0;));`[0]);
+		machine.execute(l`reset 0;`[0]);
+		assert(Value.equals(machine.evaluate(l`a`), new Value.Number(0)));
 	});
 	describe('error', function () {
 		var Error = require('../Error');
